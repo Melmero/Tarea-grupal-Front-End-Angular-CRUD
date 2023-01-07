590e67d3b1358d395bbd6c9d01491c3bed39eeb8
DaniloPV18 <damipive1809@gmail.com>
2023-01-07 14:24:09 -0500

    Actualización de módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//Componentes importados librería Angular
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+
+//Componentes creados manualmente - Proyecto.
 import { ClientesComponent } from './clientes/clientes.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AppRouterModule } from './app-router.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input'
-import {MatCardModule} from '@angular/material/card'
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatIconModule} from '@angular/material/icon';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTableModule} from '@angular/material/table';
 import { PageInicialComponent } from './page-inicial/page-inicial.component';
 import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component'
+import { ModificarClienteComponent } from './modificar-cliente/modificar-cliente.component';
 
 @NgModule({
   declarations: [
@@ -27,11 +32,12 @@ import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.compo
     FooterComponent,
     LoginComponent,
     PageInicialComponent,
-    AgregarClienteComponent
+    AgregarClienteComponent,
+    ModificarClienteComponent
   ],
   imports: [
-    BrowserModule, AppRouterModule, BrowserAnimationsModule, 
-    MatButtonModule, ReactiveFormsModule, MatInputModule, 
+    BrowserModule, AppRouterModule, BrowserAnimationsModule,
+    MatButtonModule, ReactiveFormsModule, MatInputModule,
     MatCardModule, MatToolbarModule, MatIconModule, MatDialogModule, MatTableModule
   ],
   entryComponents: [LoginComponent],
