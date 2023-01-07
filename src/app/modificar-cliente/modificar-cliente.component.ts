72ccff0cf6f50b6a89c67c6c68ff18000210c051
DaniloPV18 <damipive1809@gmail.com>
2023-01-07 14:57:31 -0500

    Form funcionalidad no Agg

--- src/app/modificar-cliente/modificar-cliente.component.ts
+++ src/app/modificar-cliente/modificar-cliente.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-modificar-cliente',
   templateUrl: './modificar-cliente.component.html',
   styleUrls: ['./modificar-cliente.component.css']
 })
-export class ModificarClienteComponent {
+export class ModificarClienteComponent implements OnInit {
+  constructor(private router: Router, private dialogRef:MatDialogRef<ModificarClienteComponent>)
+  { }
 
+  ngOnInit():void{
+  }
+
+  usuarioModificado = new FormGroup({
+    cedula: new FormControl('',Validators.required),
+    nombres: new FormControl('',Validators.required),
+    apellidos: new FormControl('',Validators.required),
+    direccion: new FormControl('',Validators.required),
+    edad: new FormControl('',Validators.required)
+  })
+
+  onSubmit(){
+
+  }
+
+  cancelar(){
+    this.dialogRef.close();
+  }
 }
