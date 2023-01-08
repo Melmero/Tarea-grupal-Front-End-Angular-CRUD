1c0a6e1f1d68d5d72fbc7a64ae2d95551981485c
DaniloPV18 <damipive1809@gmail.com>
2023-01-08 00:35:07 -0500

    Datos enviados al Dialog

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute, Router} from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { AgregarClienteComponent } from '../agregar-cliente/agregar-cliente.component';
 import { ClienteInterface } from '../interfaces/ClienteInterface';
 import { ModificarClienteComponent } from '../modificar-cliente/modificar-cliente.component';
@@ -15,45 +15,44 @@ import { ModificarClienteComponent } from '../modificar-cliente/modificar-client
 export class ClientesComponent implements OnInit {
   filtro: any;
   dataSource: any = [];
-  displayedColumns: string[] = ['cedula','nombres', 'apellidos','direccion','edad','accion']
-  
+  displayedColumns: string[] = ['cedula', 'nombres', 'apellidos', 'direccion', 'edad', 'accion']
+
   data = [{
-        cedula: '0151245245',      
-        nombres: 'Andrés Luis',
-        apellidos: 'Carvajal Lozano',
-        direccion: 'Quito, Ecuador',
-        edad: 50
-      },
-      {
-        cedula: '0954658913',      
-        nombres: 'Jorge Luis',
-        apellidos: 'Charco Aguirre',
-        direccion: 'Guayaquil, Ecuador',
-        edad: 36
-      },
-      {
-        cedula: '0957962158',      
-        nombres: 'Andrea Lisbeth',
-        apellidos: 'Romero Haro',
-        direccion: 'Guayaquil, Ecuador',
-        edad: 45
-      }
-    ];
-  
-  nuevoCliente:any;
+    cedula: '0151245245',
+    nombres: 'Andrés Luis',
+    apellidos: 'Carvajal Lozano',
+    direccion: 'Quito, Ecuador',
+    edad: 50
+  },
+  {
+    cedula: '0954658913',
+    nombres: 'Jorge Luis',
+    apellidos: 'Charco Aguirre',
+    direccion: 'Guayaquil, Ecuador',
+    edad: 36
+  },
+  {
+    cedula: '0957962158',
+    nombres: 'Andrea Lisbeth',
+    apellidos: 'Romero Haro',
+    direccion: 'Guayaquil, Ecuador',
+    edad: 45
+  }
+  ];
+
+  nuevoCliente: any;
   nav: any;
 
-  constructor(private router: Router, private dialog:MatDialog) { 
-    
+  constructor(private router: Router, private dialog: MatDialog) {
+
     this.nav = this.router.getCurrentNavigation();
     this.nuevoCliente = this.nav.extras.state;
-  
-    if (this.nuevoCliente != null)
-    {      
+
+    if (this.nuevoCliente != null) {
       console.log(this.nuevoCliente.datosCliente.queryParams);
       this.data.push(this.nuevoCliente.datosCliente.queryParams);
     }
-    
+
   };
 
   ngOnInit(): void {
@@ -61,19 +60,19 @@ export class ClientesComponent implements OnInit {
     console.log(this.data);
   }
 
-  openDialogAgregar(){
+  openDialogAgregar() {
     this.dialog.open(AgregarClienteComponent, {
       width: '50%',
     })
   }
 
-  openDialogModificar(){
+  openDialogModificar(element: ClienteInterface) {
     this.dialog.open(ModificarClienteComponent, {
-      width: '50%',
+      width: '50%', data: { client: element, tabla: this.dataSource }
     })
   }
 
-  filtrar(){
+  filtrar() {
     this.dataSource.filter = this.filtro;
   }
 }
